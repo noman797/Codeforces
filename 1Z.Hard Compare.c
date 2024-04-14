b99bc17fd2224a6cbf1e5521c34505a672a58b7b
Abdullah Al Noman <165512794+noman797@users.noreply.github.com>
2024-04-14 15:30:28 +0600

    1Z.Hard Compare.c

--- 1Z.Hard Compare.c
+++ 1Z.Hard Compare.c
@@ -1,12 +1,18 @@
-    #include<stdio.h>
-    int main(){
-        int n1,n2,n3,n4;
-        scanf("%d%d%d%d",&n1,&n2,&n3,&n4);
-        n1%=100;
-        n2%=100;
-        n3%=100;
-        n4%=100;
-        int t=n1*n2*n3*n4;
-        printf("%02d",t%100);
-        return 0;
-    }
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ 
+   long long int a,b,c,d;
+   scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
+   if(b*log(a)>d*log(c))
+   {
+    printf("YES");
+   }
+   else
+   {
+    printf("NO");
+   }
+ 
+    return 0;
+}
